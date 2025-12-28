38d0b5eb119c248767a0d13d8d2d6b8c1587bf32
muskanaggithub <muskanagarwal1901@gmail.com>
2025-12-28 14:37:35 +0530

    Update AppState.jsx

--- src/context/AppState.jsx
+++ src/context/AppState.jsx
@@ -5,7 +5,7 @@ import { ToastContainer, toast, Bounce } from 'react-toastify';
 
 const AppState = (props) => {
 
-    const url = "http://localhost:1000/api";
+    const url = "https://mern-e-commerce-api-1-l64b.onrender.com";
 
     const [products, setProducts] = useState([])
     const [token, setToken] = useState("")
